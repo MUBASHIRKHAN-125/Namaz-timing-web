67d2cf837f7244f2ed344c598b27751e413a67cc
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-05-13 14:30:15 +0500

    complete!

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 const formatChange = (valueSplit) => {
-let splittedTime = valueSplit.split(":");
-if(splittedTime[0] > 12){
-    let timeCon = splittedTime[0] - 12;
-    return `${timeCon}:${splittedTime[1]} PM`
-}else if(splittedTime[0] == 12){
-    return `${splittedTime[0]}:${splittedTime[1]} PM`
-}else{
-    return `${splittedTime[0]}:${splittedTime[1]} AM`
-}
-}
-
-const Click = () =>{
-let userCity = document.getElementById("city").value;
-let userCountry = document.getElementById("country").value;
-
-if(!userCity || !userCountry){
-    alert("Please fill the input!")
-    return;
+    let splittedTime = valueSplit.split(":");
+    if (splittedTime[0] == "00") {
+        return `12:${splittedTime[1]} AM`;
+    }
+    if (splittedTime[0] > 12) {
+        let timeCon = splittedTime[0] - 12;
+        return `${timeCon}:${splittedTime[1]} PM`
+    }
+    else if (splittedTime[0] == 12) {
+        return `${splittedTime[0]}:${splittedTime[1]} PM`
+    }
+    else {
+        return `${splittedTime[0]}:${splittedTime[1]} AM`
+    }
 }
 
-axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${userCity}&country=${userCountry}&method=1a day ago`)
-.then((timingresponse) => {
-console.log("Timing Response" , timingresponse.data);
-let short = timingresponse.data.data;
-document.getElementById("Fajr").innerText = `Fajr: ${formatChange(short.timings.Fajr)}`
-document.getElementById("Dhuhr").innerText = `Dhuhr: ${formatChange(short.timings.Dhuhr)}`
-document.getElementById("Asr").innerText = `Asr: ${formatChange(short.timings.Asr)}`
-document.getElementById("Maghrib").innerText = `Maghrib: ${formatChange(short.timings.Maghrib)}`
-document.getElementById("Isha").innerText = `Isha: ${formatChange(short.timings.Isha)}`
-document.getElementById("Date").innerText = `Date: ${short.date.readable}`
-document.getElementById("sunset").innerText = `Sunset: ${formatChange(short.timings.Sunset)}`
-document.getElementById("Imsak").innerText = `Imsak: ${short.timings.Imsak}`
-document.getElementById("Sunrise").innerText = `Sunrise: ${formatChange(short.timings.Sunrise)}`
-document.getElementById("Midnight").innerText = `Midnight: ${short.timings.Midnight}`
+const Click = () => {
+    let userCity = document.getElementById("city").value;
+    let userCountry = document.getElementById("country").value;
 
-})
-.catch((timingError) => {
-    console.log("Timing Error" , timingError);
-    alert("Timing Error");
-})
+    if (!userCity || !userCountry) {
+        alert("Please fill the input!")
+        return;
+    }
+
+    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${userCity}&country=${userCountry}&method=1a day ago`)
+        .then((timingresponse) => {
+            console.log("Timing Response", timingresponse.data);
+            let short = timingresponse.data.data;
+            document.getElementById("Fajr").innerText = `Fajr: ${formatChange(short.timings.Fajr)}`
+            document.getElementById("Dhuhr").innerText = `Dhuhr: ${formatChange(short.timings.Dhuhr)}`
+            document.getElementById("Asr").innerText = `Asr: ${formatChange(short.timings.Asr)}`
+            document.getElementById("Maghrib").innerText = `Maghrib: ${formatChange(short.timings.Maghrib)}`
+            document.getElementById("Isha").innerText = `Isha: ${formatChange(short.timings.Isha)}`
+            document.getElementById("Date").innerText = `Date: ${short.date.readable}`
+            document.getElementById("sunset").innerText = `Sunset: ${formatChange(short.timings.Sunset)}`
+            document.getElementById("Imsak").innerText = `Imsak: ${short.timings.Imsak}`
+            document.getElementById("Sunrise").innerText = `Sunrise: ${formatChange(short.timings.Sunrise)}`
+            document.getElementById("Midnight").innerText = `Midnight: ${short.timings.Midnight}`
+
+        })
+        .catch((timingError) => {
+            console.log("Timing Error", timingError);
+            alert("Timing Error");
+        })
 }
 
 const Refresh = () => {
